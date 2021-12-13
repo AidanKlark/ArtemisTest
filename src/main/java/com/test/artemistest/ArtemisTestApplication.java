923fdd7def89b6226bb7a5ea0090d7dd064b5945
AidanKlark <85440133+AidanKlark@users.noreply.github.com>
2021-12-13 23:16:50 +0300

    Init commit

---
+++ src/main/java/com/test/artemistest/ArtemisTestApplication.java
@@ -0,0 +1,14 @@
+package com.test.artemistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtemisTestApplication.class, args);
+    }
+
+}
+
